09e4dfa52b8def886f5685d291fee61bd35b7f13
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-05-01 12:48:25 +0600

    Update Khalid2.js

--- scripts/commands/Khalid2.js
+++ scripts/commands/Khalid2.js
@@ -1,27 +1,31 @@
+const fs = require('fs');
+
 module.exports.config = {
   name: "Khalid2",
-    version: "1.0.0",
-    permission: 0,
-    credits: "nayan",
-    description: "mention",
-    prefix: true,
-    category: "user",
-    usages: "tag",
-    cooldowns: 5,
+  version: "1.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "mention",
+  prefix: true,
+  category: "user",
+  usages: "tag",
+  cooldowns: 5,
 };
-module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100088679032995","100088679032995") {
-    var aid = ["100088679032995","100088679032995"];
-    for (const id of aid) {
-    if ( Object.keys(event.mentions) == id) {
-	    var msg = {
-				body: "এই নেও বাবু ফিডার খাও, আমাকে তো আর খাইতে পারবা না🤭🤭",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Fidar.mp3`)
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🍼", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-	}
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "100088679032995", "100088679032995") {
+    var aid = ["100088679032995", "100088679032995"];
+    for (const id of aid) {
+      if (Object.keys(event.mentions) == id) {
+        var msg = ["F##k You", "🖕🖕"];
+        return api.sendMessage({
+          body: msg[Math.floor(Math.random() * msg.length)],
+          attachment: fs.createReadStream(__dirname + `/Nayan/Fidar.mp3`),
+        }, event.threadID, event.messageID);
+      }
+    }
+  }
+};
+
+module.exports.run = async function({}) {
+};
