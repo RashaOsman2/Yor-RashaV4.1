6b80bd7a54585ad2dd2e8d92e4a9daeba96c97c7
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-05-01 12:40:55 +0600

    Update Khalid2.js

--- scripts/commands/Khalid2.js
+++ scripts/commands/Khalid2.js
@@ -14,15 +14,14 @@ module.exports.handleEvent = function({ api, event }) {
     var aid = ["100088679032995","100088679032995"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = {
-				body: "🖕🖕",
-				attachment: fs.createReadStream(__dirname + `/Nayan/lv_0_20240425221229.mp3`)
-      }
-      api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
-    }
-    }}
-};
-module.exports.run = function({ api, event, client, __GLOBAL }) {
+	    var msg = {
+				body: "এই নেও বাবু ফিডার খাও, আমাকে তো আর খাইতে পারবা না🤭🤭",
+				attachment: fs.createReadStream(__dirname + `/Nayan/Fidar.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🍼", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-}
+	}
